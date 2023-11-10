3fe683109de5eeebced8ae6e133adcfca78a03b9
LeonardoMontalvo <axel.montalvo186@ist17dejulio.edu.ec>
2023-11-10 16:16:55 -0500

    Semaforo

---
+++ src/semaforo/Semaforo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semaforo;
+
+/**
+ *
+ * @author Leo
+ */
+public class Semaforo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  
+    
+    }
+}
+           
